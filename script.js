95c9c4b2ae33bdaf614ee72f202aeb73a44d962b
mhiztega <oghenetegafavour39@gmail.com>
2023-04-07 04:14:40 +0100

    first task

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let number = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > number) {
-      arr[j+1] = arr[j];
-      j--;
-    }
-    arr[j+1] = current;
+function dot_product(v1, v2) {
+  if (v1.length !== v2.length) {
+    throw new Error('Vectors must have the same length');
   }
-  return arr;
+  let result = 0;
+  for (let i = 0; i < v1.length; i++) {
+    result += v1[i] * v2[i];
+  }
+  return result;
 }
+
+function is_orthogonal(v1, v2) {
+  return dot_product(v1, v2) === 0;
+}
+
+const v1 = [3, 4, 5];
+const v2 = [7, 8, 9];
+const v3 = [1, 0, -1];
+const v4 = [2, 0, 2];
+
+console.log(is_orthogonal(v1, v2)); 
+console.log(is_orthogonal(v3, v4)); 
